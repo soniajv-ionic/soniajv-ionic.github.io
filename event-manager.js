619756b2f376227218621a3d511d7872fa746d67
soniajv-ionic <soniajv.ionic@gmail.com>
2024-12-17 22:28:08 +0100

    create select listener

--- event-manager.js
+++ event-manager.js
@@ -1,12 +1,33 @@
-document.querySelector("#btn-titulo").addEventListener("click", () => {
-    clearCards();
-    const searchText = document.querySelector("#titulo").value;
-    const filteredMovies =
-        movies.filter(movie =>
-            movie.Title
-                .toUpperCase()
-                .includes(searchText.trim().toUpperCase()));
-        filteredMovies.forEach(movie => {
-        generateCard(movie);
-    });
+document.querySelector("#title-button").addEventListener("click", () => {
+  clearCards();
+  const searchText = document.querySelector("#title-input").value;
+  const filteredMovies = movies.filter((movie) =>
+    movie.Title.toUpperCase().includes(searchText.trim().toUpperCase())
+  );
+  filteredMovies.forEach((movie) => {
+    generateCard(movie);
+  });
+});
+
+document.querySelector("#actor-button").addEventListener("click", () => {
+  clearCards();
+  const searchText = document.querySelector("#actor-input").value;
+  const filteredMovies = movies.filter((movie) =>
+    movie.Actors.toUpperCase().includes(searchText.trim().toUpperCase())
+  );
+  filteredMovies.forEach((movie) => {
+    generateCard(movie);
+  });
+});
+
+document.querySelector("#genre-select").addEventListener("change", (event) => {
+  clearCards();
+  console.log("Opción seleccionada:", event.target.value);
+  const filteredMovies = movies.filter((movie) =>
+    movie.Genre.toUpperCase().includes(event.target.value.toUpperCase())
+  );
+  console.log(filteredMovies);
+  filteredMovies.forEach((movie) => {
+    generateCard(movie);
+  });
 });
