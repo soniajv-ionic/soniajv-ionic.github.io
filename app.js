11073f8d2a6c6a2f94e4bee7eceb4192ad9a2de1
soniajv-ionic <soniajv.ionic@gmail.com>
2024-12-17 10:06:12 +0100

    update movies-250.json

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const URL='https://soniajv-ionic.github.io/movies-250.json';
 
 function processMovie(data) {
-    console.log(data);
+    const movies = data.movies;
+    movies.array.forEach(m => {
+        console.log(m.Title);
+    });
+    
+    for (movie of movies) {
+        console.log(`Director: ${movie.Director}`);
+    }
 }
 
 doGetRequest(URL, processMovie);
